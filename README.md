31ea7fbe330c66db15b2cf72b8dae5347a9e762e
mamta925 <mamtarajput925@gmail.com>
2020-07-25 22:51:26 +0530

    add docker file

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+server will run on http://ec2-35-153-194-55.compute-1.amazonaws.com:3008/api-docs/
+
+please put library Id 1 by default
+
+Do authorization if you get invalid token use :   secretKey
+
+
--- validation/middleware.js
+++ validation/middleware.js
@@ -4,9 +4,9 @@ module.exports = async (req, res, next) => {
     if (req.headers.apikey == config.secretKey) {
       next();
     } else {
-      return res.status(401).json({ message: "Invalid or no token passed" });
+      return res.status(401).json({ message: "Invalid or no token passed please Authorize with secretKey" });
     }
   } else {
-    return res.status(401).json({ message: "Invalid or no token passed" });
+    return res.status(401).json({ message: "Invalid or no token passed Authorize with secretKey" });
   }
 };
