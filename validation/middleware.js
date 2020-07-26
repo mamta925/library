bbb14af61d3e235809bcd5570b7ea65edd4a5f08
mamta925 <mamtarajput925@gmail.com>
2020-07-26 21:19:56 +0530

    add docker file

--- validation/middleware.js
+++ validation/middleware.js
@@ -4,9 +4,9 @@ module.exports = async (req, res, next) => {
     if (req.headers.apikey == config.secretKey) {
       next();
     } else {
-      return res.status(401).json({ message: "Invalid or no token passed please Authorize with secretKey" });
+      return res.status(401).json({ message: "Invalid or no token passed please Authorize header  with secretKey for more INfo : go to https://github.com/mamta925/library" });
     }
   } else {
-    return res.status(401).json({ message: "Invalid or no token passed Authorize with secretKey" });
+    return res.status(401).json({ message: "Invalid or no token passed Authorize header with secretKey" });
   }
 };
